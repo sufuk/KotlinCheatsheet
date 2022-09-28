ed4c6e5c98c8ffae069c555b6e47b07b5f5fc8e8
sufuk <sufukguler@gmail.com>
2022-09-28 12:14:58 +0300

    third commit

--- src/main/kotlin/Functions.kt
+++ src/main/kotlin/Functions.kt
@@ -7,9 +7,17 @@ fun myFunction2(fname: String) {
 fun myFunction3(fname: String, age: Int) {
     println(fname + " is " + age)
 }
+fun myFunction4(x: Int): Int {
+    return (x + 5)
+}
+fun myFunction5(x: Int, y: Int) = x + y
 fun functions(){
 
     myFunction()
     myFunction2("sufuk")
     myFunction3("sufuk", 22)
+    var myvar =myFunction4( 22)
+    println(myvar)
+    println(myFunction5(2,3))
+
 }
